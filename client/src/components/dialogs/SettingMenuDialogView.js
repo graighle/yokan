2b7aec6fa3d155c77325060e7389171b5ba2af70
Shogo Furusawa <graighle@gmail.com>
2019-08-24 00:06:29 +0900

    Added sign out menu.

--- client/src/components/dialogs/SettingMenuDialogView.js
+++ client/src/components/dialogs/SettingMenuDialogView.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 const SettingMenuDialogView = props => (
 	<div className="l-setting-dialog m-setting-menu t-setting-menu">
+		<ul className="m-menu-list t-menu-list">
+			<li onClick={props.clickSignOut}>Sign out</li>
+		</ul>
 	</div>
 );
 
