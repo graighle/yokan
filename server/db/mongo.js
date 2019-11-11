d53c2f222e343dcc580b7de058a6b0c5b50fcda1
Shogo Furusawa <graighle@gmail.com>
2019-11-12 00:27:20 +0900

    Enabled MongoClient.useUnifiedTopology

--- server/db/mongo.js
+++ server/db/mongo.js
@@ -3,6 +3,11 @@ import { MongoClient } from 'mongodb';
 
 const url = 'mongodb://localhost:27017/yokan';
 
+const clientOptions = {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+};
+
 var dbCache = null;
 
 export function database(){
@@ -10,7 +15,7 @@ export function database(){
 	if(dbCache !== null)
 		return Promise.resolve(dbCache);
 
-	return MongoClient.connect(url, {useNewUrlParser:true}).then(client => {
+	return MongoClient.connect(url, clientOptions).then(client => {
 		dbCache = client.db();
 		return dbCache;
 	}).catch(err => {
